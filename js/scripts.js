87f418cc05e06194b8f97a5ba8b26bf335293c57
Dylan Lewis and Nicole Freed <nicole@>
2017-06-08 16:08:04 -0700

    add function to determine size of crypto square from input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
+// enter back-end logic here
+function square(num) {
+  numLength = num.length;
+  nearSquare = parseInt(numLength ** 0.5) + 1;
+  return nearSquare;
+}
+
+
+// enter front-end logic here
 $(document).ready(function() {
   $("#crypto").submit(function(event) {
     event.preventDefault();
-    var number = parseInt($("input#message").val());
+    var message = $("input#message").val();
     // console.log(number);
     // numCheck(number);
     // singleDigit(number);
     // multiSymbol(number);
-    $("#result").text(crypto(message));
+    $("#result").text(square(message));
   });
 });
