62f5b90f9c5aa94c9aae394c953c3cc48a2e9d8a
Nicole Freed <nicole.freed@gmail.com>
2017-06-15 07:31:55 -0700

    add code to spool message characters into two-dimensional array

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,58 @@
 // enter back-end logic here
-function alpha(msg) {
+function alpha(message) {
   // strips all non-alpha characters from a message string
-  messageAlpha = msg.replace(/[^a-z,A-Z]/g, "").toLowerCase();
+  messageAlpha = message.replace(/[^a-z,A-Z]/g, "").toLowerCase();
   return messageAlpha;
-    console.log(messageAlpha);
-}
+    // console.log(messageAlpha);
+};
 
-function square(message) {
+function square(messageAlpha) {
   // gets the size of the message square from the message length
-  messageLength = alpha(message).length;
-    console.log(message);
-    console.log(alpha(message));
-    console.log(messageLength);
+  messageLength = alpha(messageAlpha).length;
+    // console.log(message);
+    // console.log(alpha(message));
+    // console.log(messageLength);
   squareSize = parseInt(messageLength ** 0.5) + 1;
   return squareSize;
-}
+};
+
+// slice messageAlpha into squareSize slices, put each into array?
+
+function arrange(messageAlpha, squareSize) {
+  // takes string and arranges it into a mutli-dimensional array of len rows and columns
+  // var messageAlpha = alpha(message);
+  // var squareSize = square(messageAlpha);
+  var messageAlpha = "thecrowfliesatdawn";
+  var squareSize = 5;
+  var messageSquare = [];
+
+  var m = 0;
+  for (var i = 0; i < squareSize; i ++){
+    messageSquare[i] = [];
+    for (var j = 0; j < squareSize; j++) {
+      messageSquare[i].push(messageAlpha[m]);
+      m++;
+    };
+  };
+
+  // for (m = 0; m < messageAlpha.length; m += 1) {
+  //   for (i = 0; i < squareSize; i += 1); {
+  //     for (j = 0; j < squareSize; j += 1) {
+  //       messageSquare[i][j].push(messageAlpha[m]);
+  //       console.log(messageAlpha[m], messageSquare[i][j]);
+  //     };
+  //   };
+  // };
+  console.log(messageSquare);
+  return messageSquare;
+};
 
 // enter front-end logic here
 $(document).ready(function() {
   $("#crypto").submit(function(event) {
     event.preventDefault();
     var message = $("input#message").val();
-    // console.log(number);
-    // numCheck(number);
-    // singleDigit(number);
-    // multiSymbol(number);
-    $("#result").text(square(message));
+    // squareArray = arrange(alpha(message), square(messageAlpha));
+    $("#result").text(arrange("thecrowfliesatdawn", 5));
   });
 });
